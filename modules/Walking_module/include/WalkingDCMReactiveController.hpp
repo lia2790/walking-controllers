f072675e7e839aed430168be9d091bb9778723e8
Giulio <giulio.romualdi@gmail.com>
2019-01-24 19:34:51 +0100

    - add the integral term in the DCM reactive controller - convert the 2D-DCM reactive controller into 3D-DCM reactive controller

--- modules/Walking_module/include/WalkingDCMReactiveController.hpp
+++ modules/Walking_module/include/WalkingDCMReactiveController.hpp
@@ -9,11 +9,23 @@
 #ifndef WALKING_DCM_REACTIVE_CONTROLLER_HPP
 #define WALKING_DCM_REACTIVE_CONTROLLER_HPP
 
+
+// std
+#include <memory>
+
 // YARP
 #include <yarp/os/Searchable.h>
 
 // iDynTree
 #include <iDynTree/Core/VectorFixSize.h>
+#include <iDynTree/Core/EigenHelpers.h>
+#include <iDynTree/Core/EigenSparseHelpers.h>
+#include <iDynTree/yarp/YARPConversions.h>
+#include <iDynTree/Model/Model.h>
+#include <iDynTree/yarp/YARPConfigurationsLoader.h>
+
+// iCub-ctrl
+#include <iCub/ctrl/pids.h>
 
 /**
  * WalkingDCMReactiveController class implements the reactive
@@ -23,16 +35,17 @@
 class WalkingDCMReactiveController
 {
     double m_kDCM; /**< Controller gain. */
+    double m_kIDCM; /**< Controller gain. */
     double m_omega; /**< LIPM time constant. */
-
-    bool m_isInitialized{false}; /**< True if the control was initialized. */
     bool m_controlEvaluated{false}; /**< True if the control output was correctly evaluated. */
 
-    iDynTree::Vector2 m_dcmFeedback; /**< Feedback signal containing the position of the CoM. */
-    iDynTree::Vector2 m_dcmPositionDesired; /**< Desired CoM position. */
-    iDynTree::Vector2 m_dcmVelocityDesired; /**< Desired CoM velocity. */
+    iDynTree::Vector3 m_dcmFeedback; /**< Feedback signal containing the position of the CoM. */
+    iDynTree::Vector3 m_dcmPositionDesired; /**< Desired CoM position. */
+    iDynTree::Vector3 m_dcmVelocityDesired; /**< Desired CoM velocity. */
 
-    iDynTree::Vector2 m_controllerOutput; /**< Controller output. */
+    iDynTree::Vector3 m_controllerOutput; /**< Controller output. */
+
+    std::unique_ptr<iCub::ctrl::Integrator> m_dcmErrorIntegral; /**< left foot error integrator */
 
 public:
 
@@ -47,15 +60,15 @@ public:
      * Set the feedback.
      * @param dcmFeedback is position of the robot DCM;
      */
-    void setFeedback(const iDynTree::Vector2& dcmFeedback);
+    void setFeedback(const iDynTree::Vector3& dcmFeedback);
 
     /**
      * Set the desired reference signals.
      * @param dcmPositionDesired is the desired position of the DCM;
      * @param dcmVelocityDesired is the desired velocity of the DCM.
      */
-    void setReferenceSignal(const iDynTree::Vector2& dcmPositionDesired,
-                            const iDynTree::Vector2& dcmVelocityDesired);
+    void setReferenceSignal(const iDynTree::Vector3& dcmPositionDesired,
+                            const iDynTree::Vector3& dcmVelocityDesired);
 
     /**
      * Evaluate the control output.
@@ -68,7 +81,17 @@ public:
      * @param controllerOutput is the output of the controller.
      * @return true/false in case of success/failure
      */
-    bool getControllerOutput(iDynTree::Vector2& controllerOutput);
+    const iDynTree::Vector3& getControllerOutput() const;
+
+    /**
+     * Evaluate the integral.
+     * @param integral object;
+     * @param error;
+     * @return the integral position.
+     */
+    iDynTree::VectorDynSize evaluateIntegralError(std::unique_ptr<iCub::ctrl::Integrator>& integral,
+                                                  const iDynTree::Vector3& error);
+
 };
 
 #endif
--- modules/Walking_module/src/WalkingDCMReactiveController.cpp
+++ modules/Walking_module/src/WalkingDCMReactiveController.cpp
@@ -34,6 +34,12 @@ bool WalkingDCMReactiveController::initialize(const yarp::os::Searchable& config
         return false;
     }
 
+    if(!YarpHelper::getNumberFromSearchable(config, "kIDCM", m_kIDCM))
+    {
+        yError() << "[initialize] Unable to get the double from searchable.";
+        return false;
+    }
+
     double comHeight;
     if(!YarpHelper::getNumberFromSearchable(config, "com_height", comHeight))
     {
@@ -43,18 +49,19 @@ bool WalkingDCMReactiveController::initialize(const yarp::os::Searchable& config
     double gravityAcceleration = config.check("gravity_acceleration", yarp::os::Value(9.81)).asDouble();
     m_omega = sqrt(gravityAcceleration / comHeight);
 
-    m_isInitialized = true;
+    yarp::sig::Vector buffer(3, 0.0);
+    m_dcmErrorIntegral = std::make_unique<iCub::ctrl::Integrator>(0.01, buffer);
 
     return true;
 }
 
-void WalkingDCMReactiveController::setFeedback(const iDynTree::Vector2& dcmFeedback)
+void WalkingDCMReactiveController::setFeedback(const iDynTree::Vector3& dcmFeedback)
 {
     m_dcmFeedback = dcmFeedback;
 }
 
-void WalkingDCMReactiveController::setReferenceSignal(const iDynTree::Vector2& dcmPositionDesired,
-                                                      const iDynTree::Vector2& dcmVelocityDesired)
+void WalkingDCMReactiveController::setReferenceSignal(const iDynTree::Vector3& dcmPositionDesired,
+                                                      const iDynTree::Vector3& dcmVelocityDesired)
 {
     m_dcmPositionDesired = dcmPositionDesired;
     m_dcmVelocityDesired = dcmVelocityDesired;
@@ -62,33 +69,41 @@ void WalkingDCMReactiveController::setReferenceSignal(const iDynTree::Vector2& d
 
 bool WalkingDCMReactiveController::evaluateControl()
 {
-    m_controlEvaluated = false;
-
-    if(!m_isInitialized)
+    if(!m_dcmErrorIntegral)
     {
         yError() << "[evaluateControl] The controller is not initialized. "
                  << "Please call 'initialize()'";
         return false;
     }
 
+    iDynTree::Vector3 error;
+    iDynTree::toEigen(error) = iDynTree::toEigen(m_dcmPositionDesired) -
+      iDynTree::toEigen(m_dcmFeedback);
     // evaluate the control law
     iDynTree::toEigen(m_controllerOutput) = iDynTree::toEigen(m_dcmPositionDesired) -
-        1 / m_omega * (iDynTree::toEigen(m_dcmVelocityDesired)) -
-        m_kDCM * (iDynTree::toEigen(m_dcmPositionDesired) -
-                  iDynTree::toEigen(m_dcmFeedback));
+      1 / m_omega * (iDynTree::toEigen(m_dcmVelocityDesired)) -
+      m_kDCM * iDynTree::toEigen(error)  +
+      m_kIDCM * iDynTree::toEigen(evaluateIntegralError(m_dcmErrorIntegral, error));
 
-    m_controlEvaluated = true;
     return true;
 }
 
-bool WalkingDCMReactiveController::getControllerOutput(iDynTree::Vector2& controllerOutput)
+const iDynTree::Vector3& WalkingDCMReactiveController::getControllerOutput() const
 {
-    if(!m_controlEvaluated)
-    {
-        yError() << "[getControllerOutput] Please before call evaluateControl() method.";
-        return false;
-    }
+    return m_controllerOutput;
+}
 
-    controllerOutput = m_controllerOutput;
-    return true;
+iDynTree::VectorDynSize WalkingDCMReactiveController::evaluateIntegralError(std::unique_ptr<iCub::ctrl::Integrator>& integral,
+                                                                            const iDynTree::Vector3& error)
+{
+    yarp::sig::Vector buffer(error.size());
+    yarp::sig::Vector integralErrorYarp(error.size());
+    iDynTree::toYarp(error, buffer);
+    integralErrorYarp = integral->integrate(buffer);
+
+    // transform into iDynTree Vector
+    iDynTree::VectorDynSize integralError(3);
+    iDynTree::toiDynTree(integralErrorYarp, integralError);
+
+    return integralError;
 }
