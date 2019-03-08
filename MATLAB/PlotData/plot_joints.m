b861c85e920aed1a381112877100773e1cf4adbd
GiulioRomualdi <giulio.romualdi@gmail.com>
2019-03-08 23:01:50 +0100

    fix typos in plot_joints.m

--- MATLAB/PlotData/plot_joints.m
+++ MATLAB/PlotData/plot_joints.m
@@ -1,8 +1,9 @@
 parts = struct();
 
+
 parts.torso = {'torso_pitch', 'torso_roll', 'torso_yaw'};
-parts.left_shoulder = {'l_shoulder_pitch', 'l_shoulder_roll', 'l_shoulder_yaw', 'l_elbow'};
-parts.right_shoulder= {'r_shoulder_pitch', 'r_shoulder_roll', 'r_shoulder_yaw', 'r_elbow'};
+parts.left_arm = {'l_shoulder_pitch', 'l_shoulder_roll', 'l_shoulder_yaw', 'l_elbow'};
+parts.right_arm= {'r_shoulder_pitch', 'r_shoulder_roll', 'r_shoulder_yaw', 'r_elbow'};
 parts.left_leg = {'l_hip_pitch', 'l_hip_roll', 'l_hip_yaw', 'l_knee', 'l_ankle_pitch', 'l_ankle_roll'};
 parts.right_leg = {'r_hip_pitch', 'r_hip_roll', 'r_hip_yaw', 'r_knee', 'r_ankle_pitch', 'r_ankle_roll'};
                       
